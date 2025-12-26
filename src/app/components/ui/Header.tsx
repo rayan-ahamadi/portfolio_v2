453f1e90a4340774854afdf50fb03d2880d3486e
rayan-ahamadi <rayan.ahamadi@laplateforme.io>
2025-12-26 22:41:48 +0100

    feat: update image formats and add section links

--- src/app/components/ui/Header.tsx
+++ src/app/components/ui/Header.tsx
@@ -1,7 +1,5 @@
 import Container from "@/components/layout/Container";
-
-
-
+import Link from "next/link";
 
 export default function Header() {
     return <header className="fixed top-0 w-full z-50">
@@ -10,9 +8,9 @@ export default function Header() {
                 <span className="font-black font-primary text-accent text-4xl mix-blend-multiply">Rayan.dev</span>
                 <nav>
                     <ul className="font-semibold font-primary text-secondary text-lg flex gap-6">
-                        <li>Projects</li>
-                        <li>About</li>
-                        <li>Contact</li>
+                        <Link href="#works"><li>Projects</li></Link>
+                        <Link href="#about"><li>About</li></Link>
+                        <Link href="#contact"><li>Contact</li></Link>
                     </ul>
                 </nav>
             </div>
--- src/app/sections/Footer.tsx
+++ src/app/sections/Footer.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 import FleurFooter from "@/assets/vector/Fleur_Footer.svg";
 
 export default function Hero() {
-    return <section className="bg-secondary min-h-screen h-auto pt-12">
+    return <section id="contact" className="bg-secondary min-h-screen h-auto pt-12">
         <Container className="grid grid-cols-12 gap-6">
-            <FleurFooter className="absolute h-auto bottom-[15vh] pointer-events-none select-none z-0 w-128.25 left-[35vw]" />
+            <FleurFooter className="absolute h-auto bottom-[15vh] pointer-events-none select-none z-0 left-[35vw]" />
             <h2 className="w-max col-start-1 col-end-12 font-primary font-black text-accent text-[292px] uppercase tracking-tight leading-[0.70]">Contact Me</h2>
             <Link className="col-start-6 col-end-12 mt-48 w-max" href="mailto:rayanahamadi13@gmail.com">
                 <span className="text-7xl uppercase underline text-primary font-semibold">Rayanahamadi13@gmail.com</span>
