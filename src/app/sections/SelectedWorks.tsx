ae00cc86eab8a07f50f1fe878da4ef90b3bac999
rayan-ahamadi <rayan.ahamadi@laplateforme.io>
2025-12-28 13:12:14 +0100

    feat: create separate pages for Cafe Ronron and CBM Blog projects

--- src/app/sections/SelectedWorks.tsx
+++ src/app/sections/SelectedWorks.tsx
@@ -2,6 +2,8 @@ import Container from "@/components/layout/Container";
 import Image from "next/image";
 import Ellipse1 from "@/assets/vector/ellipse1.svg";
 import Ellipse2 from "@/assets/vector/ellipse2.svg";
+import Link from "next/link";
+
 
 
 export default function SelectedWorks() {
@@ -11,16 +13,19 @@ export default function SelectedWorks() {
             title: "Bloop",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
             imageUrl: "/images/bloop.webp",
+            link: "/works/bloop"
         },
         {
             title: "Café Ronron",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
             imageUrl: "/images/cafe_ronron.webp",
+            link: "/works/cafe-ronron"
         },
         {
             title: "CBM Blog",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
             imageUrl: "/images/cbm_blog.webp",
+            link: "/works/cbm-blog"
         },
     ]
 
@@ -35,13 +40,15 @@ export default function SelectedWorks() {
                 </div>
                 {projects.map((project, index) => (
                     <div key={index} className={`col-span-6 ${index % 2 === 0 ? 'col-start-1' : 'col-start-5'}`}>
-                        <Image
-                            src={project.imageUrl + `?${new Date().getTime()}`}
-                            alt={project.title}
-                            width={800}
-                            height={600}
-                            className="mb-4 h-auto w-200 object-cover"
-                        />
+                        <Link href={project.link} className="hover:opacity-70 transition-opacity duration-300">
+                            <Image
+                                src={project.imageUrl + `?${new Date().getTime()}`}
+                                alt={project.title}
+                                width={800}
+                                height={600}
+                                className="mb-4 h-auto w-200 object-cover"
+                            />
+                        </Link>
                         <div className="flex flex-col gap-3">
                             <h3 className="mt-4 text-3xl  font-semibold font-primary text-secondary uppercase">{project.title}</h3>
                             <p className="mt-2 text-2xl font-light font-primary text-secondary uppercase leading-7">{project.description}</p>
