0e692719087e102bf78a3c805f0d52f55340567a
rayan-ahamadi <rayanahamadi13@gmail.com>
2026-01-06 15:51:30 +0100

    feat: add HiddenTextReveal animation component and integrate into Hero section

---
+++ src/app/components/animations/HiddenTextReveal.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
+import { useGSAP } from "@gsap/react";
+import { useRef, useLayoutEffect, useState } from "react";
+
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+type Props = {
+    children: React.ReactNode;
+    verticalOrigin?: 'top' | 'bottom'
+    delay?: number;
+}
+
+export default function HiddenTextReveal({ children, verticalOrigin = "bottom", delay }: Props) {
+    const textRef = useRef<HTMLDivElement>(null);
+    const svgTextRef = useRef<SVGTextElement>(null);
+    const textContent = typeof children === 'string' ? children : '';
+    const [parentClasses, setParentClasses] = useState<string>("");
+
+    useLayoutEffect(() => {
+        const parentNode = textRef.current?.parentNode as HTMLElement | null;
+        if (parentNode) {
+            setParentClasses(Array.from(parentNode.classList).join(" "));
+        } else {
+            setParentClasses("");
+        }
+    }, []);
+
+    useGSAP(() => {
+        const element = textRef.current;
+        if (!element) return;
+
+        const split = new SplitText(element, {
+            type: "lines,chars",
+            charsClass: "char",
+            reduceWhiteSpace: false,
+        });
+
+        gsap.from(split.chars, {
+            yPercent: verticalOrigin === "bottom" ? 300 : -300,
+            duration: 1,
+            ease: "power4.out",
+            stagger: {
+                each: 0.02,
+            },
+            delay: delay || 0,
+            scrollTrigger: {
+                trigger: element,
+                start: "top 80%",
+            }
+        });
+    })
+
+
+    return (
+        <div ref={textRef} className="overflow-hidden inline-block h-full w-max">
+            {children}
+        </div>
+    );
+
+
+}
--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -1,28 +1,87 @@
+"use client";
+
 import Container from "@/components/layout/Container";
 import FleurHero from "@/assets/vector/Fleur_Hero.svg";
+import HiddenTextReveal from "@/components/animations/HiddenTextReveal";
+
+import { useRef } from "react";
+
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 export default function Hero() {
+    const fleurRef = useRef<SVGSVGElement>(null);
+
+    useGSAP(() => {
+        const fleur = fleurRef.current;
+        if (!fleur) return;
+
+        gsap.set(
+            fleur, {
+            transformOrigin: "50% 50%",
+        }
+        )
+
+        gsap.from(
+            fleur,
+            {
+                scale: 1.2,
+                // opacity: 1.2,
+                // y: 10,
+                rotate: 10,
+                ease: "back.out",
+                delay: 0,
+                duration: 1.5,
+            }
+        )
+    }, []);
+
+
     return <section className="relative min-h-screen h-auto bg-primary">
         <FleurHero className="absolute
-    left-1/2 top-[42%] md:top-[40%]
+    left-1/2 top-[42%] md:top-[34%]
     -translate-x-1/2 -translate-y-1/2
     w-84 md:w-128.25 h-auto
     pointer-events-none select-none
-    " />
+    "
+            ref={fleurRef}
+        />
         <Container className="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 gap-6">
             <div className="col-span-4 md:col-span-8 lg:col-span-12 flex flex-col justify-end items-start min-h-screen py-7 tracking-tight">
                 <h2 className="text-3xl md:text-4xl lg:text-5xl font-primary z-[55] md:z-0">Creative developer focused on <span className="font-accent text-accent text-[46px] md:text-[62px] lg:text-[68px] mix-blend-darken">motion</span> and <span className="font-accent text-accent text-[46px] md:text-[62px] lg:text-[68px] mix-blend-darken">structure</span></h2>
-                <h1 className="text-[92px] md:text-[218px]  lg:text-[318px] font-primary font-black text-accent uppercase leading-[0.70] mix-blend-darken">Rayan.dev</h1>
+                <h1 className="text-[92px] md:text-[218px]  lg:text-[298px] font-primary font-black text-accent fill-accent uppercase leading-[0.70] mix-blend-darken w-full relative right-1.5">
+                    <HiddenTextReveal verticalOrigin="top" delay={0.5}>
+                        Rayan.dev
+                    </HiddenTextReveal>
+                </h1>
             </div>
-            <div className="col-span-4 md:col-span-8 lg:col-span-12  text-[68px] md:text-[176px] font-primary font-extralight leading-[0.70] mix-blend-darken z-10 grid grid-cols-10 grid-rows-3 grid-flow-row gap-17 pb-20 my-32 uppercase">
-                <p className="inline w-max col-start-1 md:col-start-1">
-                    <span className="font-accent normal-case text-accent">Design</span> First
+            <div className="col-span-4 md:col-span-8 lg:col-span-12  text-[68px] md:text-[176px] mix-blend-darken z-10 grid grid-cols-10 grid-rows-3 grid-flow-row gap-17 pb-20 my-32">
+                <p className="inline w-max col-start-1 md:col-start-1 leading-none">
+                    <span className="font-accent normal-case text-accent">
+                        <HiddenTextReveal>Design</HiddenTextReveal>
+                    </span>
+                    <span className="font-primary font-extralight leading-[0.70] uppercase">
+                        &nbsp;
+                        <HiddenTextReveal>First</HiddenTextReveal>
+                    </span>
                 </p>
-                <p className="inline w-max col-start-1  md:col-start-2 row-start-2">
-                    <span className="font-accent normal-case text-accent">Code</span> Second
+                <p className="inline w-max col-start-1  md:col-start-2 row-start-2 leading-none">
+                    <span className="font-accent normal-case text-accent">
+                        <HiddenTextReveal>Code</HiddenTextReveal>
+                    </span>
+                    <span className="font-primary font-extralight leading-[0.70] uppercase">
+                        &nbsp;
+                        <HiddenTextReveal>Second</HiddenTextReveal>
+                    </span>
                 </p>
-                <p className="inline w-max col-start-1  md:col-start-3 row-start-3">
-                    <span className="font-accent normal-case text-accent">Motion</span> Always
+                <p className="inline w-max col-start-1  md:col-start-3 row-start-3 leading-none">
+                    <span className="font-accent normal-case text-accent">
+                        <HiddenTextReveal>Motion</HiddenTextReveal>
+                    </span>
+                    <span className="font-primary font-extralight leading-[0.70] uppercase">
+                        &nbsp;
+                        <HiddenTextReveal>Always</HiddenTextReveal>
+                    </span>
                 </p>
             </div>
         </Container>
